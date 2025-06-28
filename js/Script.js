7f04b3e6a547dbb7b8f613bdc1d7f48146e610f5
Surya <suryapanneer04@gmail.com>
2025-06-28 17:39:04 +0530

    .

---
+++ js/Script.js
@@ -0,0 +1,8 @@
+function toggleLanguage() {
+  document.getElementById("about-tamil").classList.toggle("hidden");
+  document.getElementById("about-english").classList.toggle("hidden");
+}
+
+function toggleTheme() {
+  document.body.classList.toggle("dark-mode");
+}
